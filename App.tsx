6bfc791a716fe27913026f9f88da64bcc94721cb
이영곤 <guest110@skylabs.io>
2025-11-19 18:03:25 +0900

    강의 3.3 완료

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { theme } from './colors';
+import { useState } from 'react';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [input, setInput] = useState("");
+
+  function onTabClick(tab: boolean) {
+    setWorking(tab);
+    setInput("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => onTabClick(true)}>
+          <Text style={{...styles.btnText, color: working?"white":theme.grey}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => onTabClick(false)}>
+          <Text style={{...styles.btnText, color: !working?"white":theme.grey}}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          value={input}
+          onChangeText={setInput}
+          placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+          style={styles.input}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +38,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+    paddingHorizontal: 20,
   },
+  header:{
+    flexDirection: 'row',
+    marginTop: 100,
+    justifyContent: 'space-between',
+  }
+  ,btnText: {
+    fontSize: 44,
+    marginHorizontal: 12,
+    fontWeight: '600',
+  }
+  ,input: {
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  }
 });
---
+++ colors.js
@@ -0,0 +1,4 @@
+export const theme = {
+    background: "#000",
+    grey: "#3a3d40"
+}
