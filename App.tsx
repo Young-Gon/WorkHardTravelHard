8a9b84f84c7957828c17a846a1443cd498d6480d
이영곤 <guest110@skylabs.io>
2025-11-20 18:10:06 +0900

    강의 3.4 완료 setState 함수 안에서는 외부에 어떠한 상태를 변경하는 함수를 추가하면 안됨

--- App.tsx
+++ App.tsx
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { theme } from './colors';
 import { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [input, setInput] = useState("");
+  const [toDos, setToDos] = useState<{[key: number]: {text: string, work:boolean}}>({});
 
   function onTabClick(tab: boolean) {
     setWorking(tab);
     setInput("");
+  }
+
+  function addToDo(): void {
+    if(input === "") {
+      return;
+    }
+    const id = Date.now();
+    const newToDos = {...toDos, [id]: {text: input, work: working}};
+    setToDos(newToDos);
+    saveToDos(newToDos);
+    setInput("");
+  }
+
+  async function saveToDos(toSave: {[key: number]: {text: string, work:boolean}}) {
+    await AsyncStorage.setItem("toDos", JSON.stringify(toSave));
   }
 
   return (
@@ -23,14 +40,23 @@ export default function App() {
           <Text style={{...styles.btnText, color: !working?"white":theme.grey}}>Travel</Text>
         </TouchableOpacity>
       </View>
-      <View>
-        <TextInput
-          value={input}
-          onChangeText={setInput}
-          placeholder={working ? "Add a To Do" : "Where do you want to go?"}
-          style={styles.input}
-        />
-      </View>
+      <TextInput
+        value={input}
+        onChangeText={setInput}
+        onSubmitEditing={addToDo}
+        returnKeyType='done'
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.entries(toDos).map(([key, value]) => 
+          value.work === working && (
+            <View key={key} style={{backgroundColor: theme.grey, padding: 20, borderRadius: 10, marginBottom: 10}}>
+              <Text style={{color: "white", fontSize: 24}}>{value.text}</Text>
+            </View>
+          )
+        )}
+      </ScrollView>
     </View>
   );
 }
